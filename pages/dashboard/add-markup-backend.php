83922b7c359198734b930f1f5522a77d61b11118
hassaanc-invents <hassaan.malik@corporate-invents.com>
2023-03-06 23:18:56 +0500

    final changes

--- pages/dashboard/add-markup-backend.php
+++ pages/dashboard/add-markup-backend.php
@@ -3,7 +3,7 @@
 if (isset($_POST['addupdatemarkup'])) {
     include "../../connection/connection.php";
     $updatedPageId = $_GET['pageId'];
-    $pageBasicStructure = $_POST['markup_data'];
+    $pageBasicStructure = mysqli_real_escape_string($conn, $_POST['markup_data']);
     $updatePageQuery = "UPDATE all_pages_markup_data SET page_markup = '{$pageBasicStructure}' WHERE page_id = '$updatedPageId'";
     $runUpdateQuery = mysqli_query($conn, $updatePageQuery);
     header("Location: $baseURL/dashboard");
